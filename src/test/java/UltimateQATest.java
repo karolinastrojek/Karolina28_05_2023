261a0c833093c7fb52dcb3f61eeede0f50204ba7
Karolina Strojek <karolinastrojek@o2.pl>
2023-05-28 14:04:16 +0200

    Test implemented

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -1,4 +1,8 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //klasa
 public class UltimateQATest extends PageSetup {
@@ -8,6 +12,13 @@ public class UltimateQATest extends PageSetup {
     // zwracanie wartości funkcji - m.in. void
     @Test
     public void testOne (){
+        WebElement buttonUsingId = driver.findElementById( "idExample");
+        buttonUsingId.click();
+
+        WebElement buttonSuccessText = driver.findElementByClassName("entry-title");
+        assertEquals("Button success", buttonSuccessText.getText());
+        
+
     }
 
 
