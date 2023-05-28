61aac47a097b1c86566149b965b4f46d223e8fec
Karolina Strojek <karolinastrojek@o2.pl>
2023-05-28 16:56:41 +0200

    Tests implemented

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -12,11 +12,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class UltimateQATest extends PageSetup {
 
 
-//metody, funkcje
+    //metody, funkcje
     // zwracanie wartości funkcji - m.in. void
     @Test
-    public void testOne (){
-        WebElement buttonUsingId = driver.findElementById( "idExample");
+    public void testOne() {
+        WebElement buttonUsingId = driver.findElementById("idExample");
         buttonUsingId.click();
 
         WebElement buttonSuccessText = driver.findElementByClassName("entry-title");
@@ -25,25 +25,25 @@ public class UltimateQATest extends PageSetup {
 
     @Test
     public void testTwo() throws InterruptedException {
-     WebElement nameTextField = driver.findElementByName("et_pb_contact_name_0");
-     nameTextField.sendKeys("Tester");
+        WebElement nameTextField = driver.findElementByName("et_pb_contact_name_0");
+        nameTextField.sendKeys("Tester");
 
-     WebElement emailTextField = driver.findElementByName("et_pb_contact_email_0");
-    emailTextField.sendKeys("tester@tester.com");
+        WebElement emailTextField = driver.findElementByName("et_pb_contact_email_0");
+        emailTextField.sendKeys("tester@tester.com");
 
-    Thread.sleep(2000);
+        Thread.sleep(2000);
 
-    WebElement emailMeButton = driver.findElementByName("et_builder_submit_button");
-    emailMeButton.click();
+        WebElement emailMeButton = driver.findElementByName("et_builder_submit_button");
+        emailMeButton.click();
 
-    Thread.sleep(2000);
+        Thread.sleep(2000);
 
-    WebElement thanksText = driver.findElementByClassName("et-pb-contact-message");
-    assertEquals("Thanks for contacting us", thanksText.getText());
+        WebElement thanksText = driver.findElementByClassName("et-pb-contact-message");
+        assertEquals("Thanks for contacting us", thanksText.getText());
     }
 
     @Test
-    public void testThree(){
+    public void testThree() {
         // xPath ogólna postać:  //*[@class='...']
         //                      //*[@class='...']/**[@id='..']
         //                     //*[contains(@class, '...')]
@@ -61,22 +61,34 @@ public class UltimateQATest extends PageSetup {
         assertTrue(bikeCheckbox.isSelected());
     }
 
-     @Test
-     public void testFive(){
+    @Test
+    public void testFive() {
 
-         //typy kolekcji  w Java: ArrayList, Map, HashList
-            List<String> listOfCars = new ArrayList<>();
-            listOfCars.add("volvo");
-            listOfCars.add("saab");
-            listOfCars.add("opel");
-            listOfCars.add("audi");
+        //typy kolekcji  w Java: ArrayList, Map, HashList
+        List<String> listOfCars = new ArrayList<>();
+        listOfCars.add("volvo");
+        listOfCars.add("saab");
+        listOfCars.add("opel");
+        listOfCars.add("audi");
 
-            //typy pętli w Java
-            for (int i = 0; i < listOfCars.size(); i++) {
+        //typy pętli w Java
+        for (int i = 0; i < listOfCars.size(); i++) {
 
-                WebElement dropdownOption = driver.findElementByXPath("//*[@value='" + listOfCars.get(i).toLowerCase() + "']");
-                dropdownOption.click();
-                assertTrue(dropdownOption.isSelected());
-            }
-            }
+            WebElement dropdownOption = driver.findElementByXPath("//*[@value='" + listOfCars.get(i).toLowerCase() + "']");
+            dropdownOption.click();
+            assertTrue(dropdownOption.isSelected());
+        }
+    }
+
+    @Test
+    public void testSix(){
+
+    WebElement buttonUsingXpath1 = driver.findElementByXPath("//span[text()='XPath tutorial for automation testers']/ancestor::h4/following-sibling::div//button");
+    buttonUsingXpath1.click();
+
+    WebElement buttonSuccessText = driver.findElementByClassName("entry-title");
+
+    assertEquals("Button success",buttonSuccessText.getText());
+}
+
 }
